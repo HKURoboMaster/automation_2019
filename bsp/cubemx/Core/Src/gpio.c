607af92c13d99facb761c70f8a77d542f50ef7ee
liu David <liuyunhao1578@gmail.com>
2019-07-02 23:26:32 +0800

    Laser pin and Orientation

--- bsp/cubemx/Core/Src/gpio.c
+++ bsp/cubemx/Core/Src/gpio.c
@@ -162,12 +162,12 @@ void MX_GPIO_Init(void)
   /*------ Added by Y.H. Liu, for laser------*/
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = LASER_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LASER_GPIO_PORT, &GPIO_InitStruct);
 
-  HAL_GPIO_WritePin(LASER_GPIO_PORT, LASER_PIN, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(LASER_GPIO_PORT, LASER_PIN, GPIO_PIN_RESET);
 }
 
 /* USER CODE BEGIN 2 */
