9d2295d6468bdb07763552c639baff74eb146e95
Eric Chen <ericcsr@connect.hku.hk>
2019-07-26 23:32:40 +0800

    demo

--- application/gimbal_task.c
+++ application/gimbal_task.c
@@ -53,6 +53,8 @@ static ramp_t pitch_ramp = RAMP_GEN_DAFAULT;
 // Orientation debugging
 int32_t mpu_pit, mpu_yaw, mpu_rol;
 int32_t mpu_wx, mpu_wy, mpu_wz;
+int target_lost_cnt_pit = 0;
+int target_lost_cnt_yaw = 0;
 // PID debugging
 int32_t yaw_angle_fdb_js, yaw_angle_ref_js;
 int32_t pit_angle_fdb_js, pit_angle_ref_js;
@@ -149,11 +151,11 @@ void gimbal_task(void const *argument)
         if(prc_info->mouse.r || rc_device_get_state(prc_dev, RC_S2_UP) == RM_OK)
         {
           if(auto_aiming_pitch!=0)
-            gimbal_set_pitch_delta(pgimbal, auto_aiming_pitch-pitch_autoaim_offset);
+           gimbal_set_pitch_speed(pgimbal,auto_aiming_pitch);
           if(auto_aiming_yaw!=0)
-            gimbal_set_yaw_delta(pgimbal, auto_aiming_yaw-yaw_autoaim_offset);
-          auto_aiming_pitch = 0;
-          auto_aiming_yaw = 0;
+           gimbal_set_yaw_speed(pgimbal, auto_aiming_yaw);
+          //auto_aiming_pitch = 0;
+          //auto_aiming_yaw = 0;
         }
 
         float square_ch1 = (float)prc_info->ch1 * abs(prc_info->ch1) / RC_CH_SCALE;
@@ -209,8 +211,7 @@ void gimbal_task(void const *argument)
     }
     if(rc_device_get_state(prc_dev, RC_S2_DOWN) == RM_OK)
     {
-      //disbaled
-			shoot_set_fric_speed();
+      //disbaled shoot_set_fric_speed();
       gimbal_pitch_disable(pgimbal);
       gimbal_yaw_disable(pgimbal);
     }
